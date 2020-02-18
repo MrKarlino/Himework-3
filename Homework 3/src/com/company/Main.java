3a6a5031a8f397fde33d1e1f83dcd1f1eb27e021
Lina <linokarlino@mail.ru>
2020-02-18 13:07:19 +0300

    Создан код с изменением баланса

---
+++ Homework 3/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int currentAmount = 2000000000;
+	int transferAmount = 500000000;
+	int finalAmount = currentAmount + transferAmount;
+	System.out.println("Final amount: " + finalAmount);
+    }
+}
